33c62b25cbc2b98547fb75f92bf5f86214b4d275
Moataz <moataz000040@gmail.com>
2026-05-20 06:03:52 +0100

    Added version 2 message

--- src/main/java/model/ManveProject1.java
+++ src/main/java/model/ManveProject1.java
@@ -14,7 +14,7 @@ public class  ManveProject1{
         //System.out.println("System Ready!");
        UserManager um = new UserManager();
         um.register("Test User", "test@gmail.com", "1234", "customer");
-        System.out.println("Hello");
+        System.out.println("Version 2");
 
 
 }
